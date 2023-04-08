32e2a8db48702bdef06937facd4c194548798964
JesseMiami100 <jessemetayer@gmail.com>
2023-04-08 19:57:16 +0000

    message

--- src/models.py
+++ src/models.py
@@ -26,9 +26,59 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable = False)
+    firstname = Column(String(250), nullable = False)
+    lastname = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+
+    follower = relationship("Follower")
+    comment = relationship("Comment")
+    post = relationship("Post")
+
     def to_dict(self):
         return {}
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    media = relationship("Media")
+    comment = relationship("Comment")
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id"))
+    type = Column(String(250), nullable = False)
+    url = Column(String(250), nullable = False)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id"))
+    type = Column(String(250), nullable = False)
+    url = Column(String(250), nullable = False)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
